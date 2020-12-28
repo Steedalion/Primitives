d0d16bce2f54e437940b8b401a24fe4f470b31ad
STEEDCLINT ALEX <steed@unist.ac.kr>
2020-12-28 14:36:54 +0900

    movement implemented.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,9 +9,13 @@ public class PlayerController : MonoBehaviour
 	Rigidbody rb;
 	public float jumpingForce;
 	public float moveForce;
+	public float limitZ;
+	Transform _transform;
+	bool isOnGround;
     // Start is called before the first frame update
 	void Awake()
 	{
+		_transform = transform;
 		rb = GetComponent<Rigidbody>();
 		input = new InputControl();
 		input.Player.Jump.performed += x =>Jump();
@@ -22,17 +26,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+	    if(transform.position.z > limitZ)
+	    {
+		     	transform.position = new Vector3(transform.position.x, transform.position.y, limitZ);
+	    }
     }
 	void Jump()
 	{
 		Debug.Log("Jump");
-		rb.AddForce(transform.up * jumpingForce);
+		if(isOnGround){
+			rb.AddForce(Vector3.up * jumpingForce);}
 	}
 	void Move(Vector2 movement)
 	{
+		Vector3 move3 = new Vector3(movement.x,0, movement.y);
 		
-		rb.AddForce(movement * moveForce);
+		rb.AddForce(move3 * moveForce);
+		
+	}
+	// OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider.
+	protected IEnumerator OnCollisionEnter(Collision collisionInfo)
+	{
+		if(collisionInfo.other.CompareTag("Ground"))
+		{
+			isOnGround = true;
+		}
+		yield return null;
+	}
+	
+	
+	// OnCollisionExit is called when this collider/rigidbody has stopped touching another rigidbody/collider.
+	protected IEnumerator OnCollisionExit(Collision collisionInfo)
+	{if(collisionInfo.other.CompareTag("Ground"))
+	{
+		isOnGround = false;
+	}
+		yield return null;
 	}
 	// This function is called when the object becomes enabled and active.
 	protected void OnEnable()
